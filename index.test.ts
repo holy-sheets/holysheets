6d66efbf228c1ad1b5fff346bb9d5d4b25b20e6b
José Teles <josetelesmaciel@gmail.com>
2024-05-19 10:57:18 -0300

    test: add initial tests

--- index.test.ts
+++ index.test.ts
@@ -1,17 +1,66 @@
+// import { sheets_v4 } from 'googleapis'
 import HollySheets from './index'
+// import { JWT } from 'google-auth-library';
+
+function mockGoogleApis(options: {getValue?: string[][], batchGetValue?: string[][], batchUpdateValue?: string[][]}) {
+  const { getValue, batchGetValue, batchUpdateValue } = options;
+  jest.mock('googleapis', () => {
+    return {
+      google: {
+        sheets: jest.fn(() => ({
+          spreadsheets: {
+            values: {
+              get: jest.fn(() => Promise.resolve(getValue ? {data: {values: getValue}} : {data: {values: [[]]}})),
+              batchGet: jest.fn(() => Promise.resolve(batchGetValue ? {
+                data: {
+                  valueRanges: [
+                    {
+                      range:"Users!A2:B2",
+                      majorDimension:"ROWS",
+                      values:batchGetValue
+                    }
+                  ]
+                }
+              }: {data: {valueRanges: []}})),
+              batchUpdate: jest.fn(() => Promise.resolve(batchUpdateValue ? batchUpdateValue : {data: {}})),
+            },
+          },
+        })),
+      },
+    };
+  });
+}
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('HollySheets', () => {
   const credentials = {
     clientEmail: 'test@example.com',
-    privateKey: 'private_key',
-    spreadsheetId: 'spreadsheet_id'
-  }
+    privateKey: 'test-private-key',
+    spreadsheetId: 'spreadsheet-id',
+  };
+  
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  const hollySheets = new HollySheets(credentials)
+  it('should initialize HollySheets with credentials', () => {
+    const hollySheets = new HollySheets(credentials);
+    expect(hollySheets).toBeTruthy();
+  });
 
-  describe('insert', () => {
-    it('should insert data into the spreadsheet', async () => {
-      
-    })
+  it('should set table with base method', () => {
+    const hollySheets = new HollySheets(credentials);
+    const table = 'TestTable'
+    const table2 = 'TestTable2'
+    expect(hollySheets.table).not.toBe(table);
+    const baseInstance = hollySheets.base(table);
+    expect(baseInstance.table).toBe(table);
+    expect(hollySheets.table).not.toBe(table);
+    const baseInstance2 = hollySheets.base(table2);
+    expect(baseInstance2.table).toBe(table2);
   })
-})
+
+});
