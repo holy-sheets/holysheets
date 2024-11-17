f07ad7b2daa984d2d2bfffd242da595e51d8d069
José Teles <josetelesmaciel@gmail.com>
2024-11-17 09:13:55 -0300

    feat: add use example for OAuth
    #5

---
+++ .examples/oauth.ts
@@ -0,0 +1,41 @@
+import { google } from 'googleapis'
+import HolySheets from '../src/index'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const spreadsheetId = process.env.SPREADSHEET_ID as string
+const clientId = process.env.OAUTH_CLIENT_ID as string
+const clientSecret = process.env.OAUTH_CLIENT_SECRET as string
+const redirectUri = process.env.OAUTH_REDIRECT_URI as string
+const refreshToken = process.env.OAUTH_REFRESH_TOKEN as string
+
+async function testOAuth() {
+  const auth = new google.auth.OAuth2(clientId, clientSecret, redirectUri)
+  auth.setCredentials({ refresh_token: refreshToken })
+
+  const sheets = new HolySheets({
+    spreadsheetId,
+    auth
+  })
+
+  const table = sheets.base<{ Name: string; Age: string }>('holysheets')
+
+  try {
+    // Insert data
+    await table.insert({
+      data: [{ Name: 'Bob', Age: '25' }]
+    })
+    console.log('Data successfully inserted using OAuth.')
+
+    // Fetch data
+    const record = await table.findFirst({
+      where: { Name: 'Bob' }
+    })
+    console.log('Record found:', record)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+testOAuth()
