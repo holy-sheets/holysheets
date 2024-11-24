5835c9c3ca2751b9493e1494dc0ef5d7a13c94ac
José Teles <josetelesmaciel@gmail.com>
2024-11-24 17:23:52 -0300

    refactor(sheetrecord): add row and range properties to SheetRecord interface #10

--- src/types/sheetRecord.ts
+++ src/types/sheetRecord.ts
@@ -4,5 +4,7 @@
  * @typeparam RecordType - The type of the fields in the record.
  */
 export interface SheetRecord<RecordType extends Record<string, any>> {
+  row: number
+  range: string
   fields: Partial<RecordType>
 }
