5f885bf581716b13b73d9a32ba7e0a5f5c39699b
José Teles <josetelesmaciel@gmail.com>
2024-12-09 08:30:03 -0300

    chore: update .npmignore and remove unused configuration files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    collectCoverage: true,
-    coverageReporters: ['text', 'lcov'],
-    coverageDirectory: 'coverage',
-};
